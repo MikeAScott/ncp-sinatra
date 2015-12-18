3f81e9f1e2bddeed809cfd098c9cd2c6cde28b66
Mike Scott <mike.scott@sqs.com>
2015-12-18 01:53:50 +0000

    Setup site skeleton

---
+++ app.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def logged_in?
+    !session[:identity].nil?
+  end
+
+  def username
+    session[:identity]
+  end
+
+  def site_title
+    'No Change Parking'
+  end
+end
+
+get '/' do
+  erb "<h1>#{site_title}</h1>"
+end
+
+get '/login' do
+  erb :login_form
+end
+
+post '/login' do
+  session[:identity] = params['username']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+get '/logout' do
+  session.clear
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+
+
